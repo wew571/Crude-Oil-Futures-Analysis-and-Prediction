586ff21186a193d6cf8784832620ed5f52b9fe5c
wew57 <huy1872006k@gmail.com>
2025-11-14 18:26:45 +0700

    Loại bỏ 2 hàm entropy và gini impurity
    Vì 2 hàm đấy thuộc Classify mà bài này chỉ sử dụng Regression nên chúng k cần

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -15,57 +15,6 @@ class DecisionTree:
         self.min_split = min_split
         self.root = None
 
-    def gini_impurity(self , y) -> float:
-        """
-        Tính Gini Impurity cho 1 node
-
-        Parameters:
-            y : array-like
-
-        Returns:
-            value : float
-
-        Công thức toán học :
-
-            G = 1 - Σ[P_i²]
-
-            G       Geni Impurity
-            P_i     Xác suất
-        """
-        if len(y) == 0:
-            return 0
-        count = numpy.bincount(y)
-        probability = count / len(y)
-
-        value = 1 - numpy.sum(probability**2)
-        return value
-
-    def entropy(self , y) -> float:
-        """
-        Tính Entropy cho 1 node:
-
-        Parameters:
-            y : array-like
-
-        Returns:
-            value : float
-
-        Công Thức Toán Học:
-
-            E = - Σ[ P_i x log2(P_i) ]
-
-            E       Entropy
-            P_i     Xác suất
-        """
-        if len(y) == 0:
-            return 0
-        count = numpy.bincount(y)
-        probability = count / len(y)
-
-        probability = probability[probability > 0]
-        value = -1 * numpy.sum(probability * numpy.log2(probability))
-        return value
-
     def mse(self , y) -> float:
         """
         Tính MSE ( Mean Square Error ) cho 1 node:

9a8a504ac2f6a98d930477546d1fadd175454c66
wew57 <huy1872006k@gmail.com>
2025-11-15 20:38:49 +0700

    Train thử mô hình
    Refactor Test.py to use Random Forest regression and read data from hist_crude_oil.

--- Test.py
+++ Test.py
@@ -2,30 +2,28 @@ import pandas
 import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
+from timeit import default_timer
 
 import decision_tree_regression
 import correlation_matrix
 import hist_crude_oil
+import random_forest_regression
 
-df = pandas.read_csv('Crude_Oil_Futures.csv')
+df = hist_crude_oil.get_data()
 
-print(correlation_matrix.corr(df , 'spearman'))
+# X_train = numpy.array([[1], [2], [3], [4], [5], [6]])
+# y_train = numpy.array([1.1, 1.9, 3.0, 4.1, 4.9, 6.0])
 
-plt.figure(figsize=(5 , 5))
-sns.heatmap(correlation_matrix.corr(df , 'spearman') , annot=True , cmap = 'coolwarm')
-plt.show()
-X_train = numpy.array([[1], [2], [3], [4], [5], [6]])
-y_train = numpy.array([1.1, 1.9, 3.0, 4.1, 4.9, 6.0])
+Xr_train = df['open'].to_numpy().reshape(-1,1)
 
+yr_train = df['close'].to_numpy()
 
-# Train cây
-tree = decision_tree_regression.DecisionTree(max_depth=2)
-tree.fit(X_train, y_train)
+start = default_timer()
+rf = random_forest_regression.RandomForestRegression(max_depth=9)
+rf.fit(Xr_train, yr_train)
 
-# Test dự đoán
-X_test = numpy.array([[1.5], [3.5], [5.5]])
-y_pred = tree.predict(X_test)
+yr_pred = rf.predict(Xr_train)
+finish = default_timer()
 
-print("Dự đoán:", y_pred)
-
-hist_crude_oil.draw()
+print(rf.r2_score(yr_train, yr_pred))
+print(finish - start)
